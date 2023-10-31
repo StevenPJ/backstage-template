67592f31579dc22a0d752bcf4a87fb6069c6b4d6
StevenPJ <13919159+StevenPJ@users.noreply.github.com>
2023-10-31 12:23:30 +0000

    Setup app chart

---
+++ hooks/post_gen_project.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+set -eo pipefail
+
+if ! command -v helm &> /dev/null
+then
+    echo "helm could not be found, installing now"
+    HOMEBREW_NO_AUTO_UPDATE=1 brew install helm
+fi
+
+if ! command -v helm-docs &> /dev/null
+then
+    echo "helm-docs could not be found, installing now"
+    if [ -z "${CI}" ]
+    then
+      HOMEBREW_NO_AUTO_UPDATE=1 brew install norwoodj/tap/helm-docs
+    else
+      GO111MODULE=on go install github.com/norwoodj/helm-docs/cmd/helm-docs@v1.11.2
+    fi
+fi
+
+# shellcheck disable=SC2164
+# shellcheck disable=SC1083
+cd charts/test
+
+# build dependencies and Chart.lock
+helm dependency build .
+
+# Grab the latest copy of the value.yaml of the upstream chart
+helm repo add mettle https://mettle-charts.storage.googleapis.com
+# shellcheck disable=SC2164
+# shellcheck disable=SC1083
+helm show values mettle/backend-generic > values.yaml --version "$(helm dependency list | awk '{ print $2 }'  | tail -n +2)"
+
+# Configure the upstream chart for our app chart
+# Change the image tag to a glob that CI will update
+yq -e -i '.application.image.tag = "REPLACE_ME"' values.yaml
+# Do not create crds by default
+yq -e -i '.imageTag.createCRDs = false' values.yaml
+# Update the image repository to the service name
+yq -e -i '.application.image.repository = "eu.gcr.io/eevee-bank/test"' values.yaml
+# Put the upstream values under the dependency name, so the values apply to the subchart
+yq -e -i '{"backend-generic": .}' values.yaml
+
+# Build docs for the app chart using README.md.gotmpl
+helm-docs
+
+
+cd ../../../
+pwd
+# shellcheck disable=SC2164
+# shellcheck disable=SC1083
+cp -r test_app_chart/charts .
+# shellcheck disable=SC2164
+# shellcheck disable=SC1083
+cp -r test_app_chart/.github .
+# shellcheck disable=SC2164
+# shellcheck disable=SC1083
+rm -rf test_app_chart
+
+echo "App chart created, see usage info in charts/test/README.md"
